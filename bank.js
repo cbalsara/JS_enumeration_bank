a69b0e79d0cce13442e010751881aee3deafd2e1
Cyrus <cdbalsara@icloud.com>
2017-02-26 16:24:53 +0000

    created and tested checkAccounts method to either return length of accounts array, or return string declaring nothing to return

--- bank.js
+++ bank.js
@@ -3,4 +3,13 @@ var Bank = function(bank_name){
   this.accounts = []
 };
 
+Bank.prototype = {
+  checkAccounts: function(){
+    if(this.accounts = undefined|| this.accounts.length < 1){
+      return "Sorry, no accounts to show"
+    }
+    else return this.accounts.length
+  }
+};
+
 module.exports = Bank;
--- specs/bank_spec.js
+++ specs/bank_spec.js
@@ -3,13 +3,17 @@ var Bank = require("../bank");
 
 describe("Bank", function(){
   before(function(){
-    bank1 = new Bank("RBS");
+    bank1 = new Bank("RBS", []);
   });
 
 it("should have a name", function(){
   assert.equal("RBS", bank1.name);
-});
+}),
 
+it("should have no accounts in it", function(){
+  managerResponse = bank1.checkAccounts();
+  assert.equal("Sorry, no accounts to show", managerResponse)
+})
 
 
 
